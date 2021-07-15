e05d86488eaba01b025eefab283b20f9a6b64fd5
Alexander Ziyatdinov <50361221+AlexanderZiyatdinov@users.noreply.github.com>
2021-07-15 17:20:11 +0500

    Add files via upload

---
+++ Week1/HW/Contest.py
@@ -0,0 +1,9 @@
+def contest(num1: int, num2: int) -> int:
+    max_num: int = ...  # ╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ
+    return max_num
+
+
+if __name__ == "__main__":
+    num1: int = int(input())
+    num2: int = int(input())
+    print(contest(num1, num2))
