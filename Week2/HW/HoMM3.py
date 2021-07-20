1259f9f03b68d012d2c50153c9ebdbd93fa130fe
Alexander Ziyatdinov <50361221+AlexanderZiyatdinov@users.noreply.github.com>
2021-07-20 12:14:16 +0500

    Add files via upload

---
+++ Week2/HW/HoMM3.py
@@ -0,0 +1,19 @@
+import towns
+
+# Ваша задача:
+# Расположить всех существ по своим городам и по своим уровням
+# Пользуйтесь статьей: https://homm3sod.ru/units/
+
+
+castle = towns.castle
+rampart = towns.rampart
+tower = towns.tower
+inferno = towns.inferno
+necropolis = towns.necropolis
+dungeon = towns.dungeon
+stronghold = towns.stronghold
+fortress = towns.fortress
+conflux = towns.conflux
+
+# TODO поправьте каждый город!
+# Ваш код напишите ниже: ╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ
